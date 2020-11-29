54c09b276dc82eeeecee80283b5b205c0ffeacea
Eric Vondunn <ericvondunn40@gmail.com>
2020-11-29 17:57:07 -0500

    switch statement and connection

--- db/index.js
+++ db/index.js
@@ -9,8 +9,7 @@ class DB {
   //find all employees and join / display roles
   findAllEmployees() {
     return this.connection.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee WHERE id != ?",
-      employeeID
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
     );
   }
 
@@ -20,4 +19,84 @@ class DB {
       employeeID
     );
   }
+
+  //create employee
+  createEmployee(employee) {
+    return this.connection.query("INSERT INTO employee SET ?", employee);
+  }
+
+  //remove employee by id
+  removeEmployee(employeeID) {
+    return this.connection.query(
+      "DELETE FROM employee WHERE id = ?",
+      employeeID
+    );
+  }
+
+  //update specific employee role
+  updateEmployeeRole(employeeID, roleId) {
+    return this.connection.query(
+      "UPDATE employee SET role_id = ? WHERE id = ?",
+      [roleID, employeeId]
+    );
+  }
+
+  //update employees management
+
+  updateEmployeeManager(employeeId, managerId) {
+    return this.connection.query(
+      "UPDATE employee SET manager_id = ? WHERE id = ?",
+      [managerId, employeeId]
+    );
+  }
+
+  //find all roles
+  findAllRoles() {
+    return this.connection.query(
+      "SELECT role.id, role.title, department.name AS department, role.salary"
+    );
+  }
+
+  //create new role
+
+  createRoll(role) {
+    return this.connection.query("INSERT INTO role SET ?", role);
+  }
+
+  //find all departments
+
+  findAllDepartments() {
+    return this.connection.query(
+      "SELECT department.id, department.name, SUM(role.salary AS utilized_b"
+    );
+  }
+
+  //create a new department
+  createDepartment(department) {}
+
+  removeDepartment(departmentId) {
+    return this.connection.query(
+      "DELETE FROM department WHERE id = ?",
+      departmentId
+    );
+  }
+
+  //not done
+  //find all employees by department
+  findAllEmployeesByDepartment(departmentId) {
+    return this.connection.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title",
+      departmentId
+    );
+  }
+
+  //not done
+  findAllEmployeesByManager(managerId) {
+    return this.connection.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, department.name",
+      managerId
+    );
+  }
 }
+
+module.exports = new DB(connection);
